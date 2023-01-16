b40ea7fc3378b163ae2a4a8b22084c7ace68bdcd
Belyaev, Ivan <ivan.belyaev@dlr.de>
2023-01-16 13:46:24 +0100

    commented client and server scripts

--- client.py
+++ client.py
@@ -12,12 +12,18 @@ from game.generator import Generator
 
 import threading
 
+# this script is the main client implementation
+# requires on mac accesibility configuration in
+# settings and also privacy tab
+# client.py should be run after the server.py
+
 if __name__ == '__main__':
     print("[CLIENT] Starting client")
     transport = TransportClient(ip="127.0.0.1")
     dispatcher = Dispatcher(transport)
     generator = Generator()
 
+    # will be called on a publish to /grid_dim
     def grid_dim_callback(msg):
         print("[CLIENT] Got grid dimensions from server!")
         width, heigt = msg.get_size()
@@ -25,6 +31,8 @@ if __name__ == '__main__':
         dispatcher.publish(msg=StartGameMsg(), topic="/start")
     dispatcher.subscribe(msg_type=GridDimensionsMsg, cb=grid_dim_callback, topic="/grid_dim")
 
+    # timer for generating and sending food position
+    # to the server
     def timer_callback():
         point = generator.get_random_position()
         fp_msg = FoodPositionMsg(point.x, point.y)
@@ -32,6 +40,7 @@ if __name__ == '__main__':
         threading.Timer(5, timer_callback).start()
     timer_callback()
 
+    # configuration of user controls
     user = User()
     up = lambda: dispatcher.publish(msg=ControlMsg(Control.UP), topic="/control")
     down = lambda: dispatcher.publish(msg=ControlMsg(Control.DOWN), topic="/control")
@@ -41,7 +50,9 @@ if __name__ == '__main__':
     user.set_callbacks(up, down, right, left)
     user.start()
     print("[CLIENT] Use following key for controlling the snake:")
-    print("         W for UP, S for DOWN, A for LEFT, D for RIGHT movements")
+    print("[CLIENT] W for UP, S for DOWN, A for LEFT, D for RIGHT movements")
+    print("[CLIENT] Exit with ctrl-c!")
 
+    # infinite loop to let the program run its threads
     while True:
         pass
--- server.py
+++ server.py
@@ -12,31 +12,41 @@ from dispatcher.dispatcher import Dispatcher
 from game.game_state import GameState
 
 
+# this script is the main server implementation
+# should be called first
 if __name__ == '__main__':
     dispatcher = Dispatcher(TransportServer())
     renderer = Renderer(width=20, height=20)
     game = Game(renderer.get_canvas_dimensions())
 
+    # add food on the message arrived at /food topic
     def food_position_callback(msg):
         if game.get_state() == GameState.RUNNING:
             game.add_food(msg.get_position())
 
+    # listen for food messages on /food topic
     dispatcher.subscribe(msg_type=FoodPositionMsg,
                          cb=food_position_callback,
                          topic="/food")
 
+    # listen for user control messages on /control topic
     dispatcher.subscribe(msg_type=ControlMsg,
                          cb=lambda msg: game.control(msg.get_control()),
                          topic="/control")
 
+    # listen to game start message on /start
+    # the client starts the game
     dispatcher.subscribe(msg_type=StartGameMsg, cb=lambda msg: game.start(), topic="/start")
 
+    # main game loop
     print("[SERVER] Waiting for client to start up!")
     while True:
+        # sending grid dimensions to the client until the client start the game
         if game.get_state() == GameState.IDLE:
             grid_dim_msg = GridDimensionsMsg(*renderer.get_canvas_dimensions())
             dispatcher.publish(msg=grid_dim_msg, topic="/grid_dim")
             sleep(1)
+        # main game update and render loop
         elif game.get_state() == GameState.RUNNING:
             game.update()
             renderer.clear()
@@ -44,8 +54,10 @@ if __name__ == '__main__':
                 renderer.add(fields)
             renderer.draw()
             sleep(0.5)
+        # on game stop break the loop and exit
         elif game.get_state() == GameState.STOPPED:
             break
 
     print()
     print("[SERVER] End of game!")
+    print("[SERVER] Exit with ctrl-c!")
